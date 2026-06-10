900e34abd9a5f43232275343ccf67c2fa50dafbe
miguelgarciaa1028 <Miguel.Garcia34@correo.tdea.edu.co>
2026-06-09 23:52:32 -0500

    frontend: use default import for useAuth in Home.jsx and update production build assets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import useAuth from '../hooks/useAuth';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
